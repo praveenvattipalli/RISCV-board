468b37caa2574d262a58f2157e9928bdbbef65e7
praveen kumar <praveen.kumar@microchip.com>
2020-01-09 16:59:20 +0530

    added build directory
    Signed-off-by: praveen kumar <praveen.kumar@microchip.com>

---
+++ build/build_all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+PRJ_NAME="aloelite"
+
+echo "Start Building Bootloader"
+bitbake virtual/bootloader
+
+echo "Start Building Kernel"
+bitbake virtual/kernel 
+
+
+echo "Started cleaning ${PRJ_NAME}-image"
+bitbake -c cleansstate ${PRJ_NAME}-image
+
+echo "Start Building ${PRJ_NAME} Image"
+bitbake -f ${PRJ_NAME}-image
+
+cp tmp/deploy/images/${PRJ_NAME}/${PRJ_NAME}-image-freedom-u540.tar.gz aloelite_images/
+cp tmp/deploy/images/${PRJ_NAME}/u-boot-${PRJ_NAME}.bin aloelite_images/u-boot.bin
+cp tmp/deploy/images/${PRJ_NAME}/bbl.bin aloelite_images/
+
