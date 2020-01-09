9f95784ab8a4dbe7860697a6509f37d8769574c9
praveen kumar <praveen.kumar@microchip.com>
2020-01-09 17:55:45 +0530

    modified build_all.sh
    Signed-off-by: praveen kumar <praveen.kumar@microchip.com>

--- build/build_all.sh
+++ build/build_all.sh
@@ -14,7 +14,7 @@ bitbake -c cleansstate ${PRJ_NAME}-image
 echo "Start Building ${PRJ_NAME} Image"
 bitbake -f ${PRJ_NAME}-image
 
-cp tmp/deploy/images/${PRJ_NAME}/${PRJ_NAME}-image-freedom-u540.tar.gz aloelite_images/
+cp tmp/deploy/images/${PRJ_NAME}/${PRJ_NAME}-image-${PRJ_NAME}.tar.gz aloelite_images/
 cp tmp/deploy/images/${PRJ_NAME}/u-boot-${PRJ_NAME}.bin aloelite_images/u-boot.bin
 cp tmp/deploy/images/${PRJ_NAME}/bbl.bin aloelite_images/
 
