547d57b7de4570c394912d6cc1fb0b00a3b9cbff
praveen kumar <praveen.kumar@microchip.com>
2020-01-23 14:39:59 +0530

    removed -f for aloelite-image
    Signed-off-by: praveen kumar <praveen.kumar@microchip.com>

--- build/build_all.sh
+++ build/build_all.sh
@@ -12,7 +12,7 @@ echo "Started cleaning ${PRJ_NAME}-image"
 bitbake -c cleansstate ${PRJ_NAME}-image
 
 echo "Start Building ${PRJ_NAME} Image"
-bitbake -f ${PRJ_NAME}-image
+bitbake  ${PRJ_NAME}-image
 
 cp tmp/deploy/images/${PRJ_NAME}/${PRJ_NAME}-image-${PRJ_NAME}.tar.gz aloelite_images/
 cp tmp/deploy/images/${PRJ_NAME}/u-boot-${PRJ_NAME}.bin aloelite_images/u-boot.bin
